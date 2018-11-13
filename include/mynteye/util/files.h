ee8529dfaf4ee1884dcd3200270672596a73caa7
John Zhao <johnzhao@slightech.com>
2018-11-13 15:09:21 +0800

    Remove useless files.h

--- include/mynteye/util/files.h
+++ include/mynteye/util/files.h
@@ -1,9 +1,24 @@
-#ifndef MYNTEYE_FILES_H // NOLINT
-#define MYNTEYE_FILES_H
+// Copyright 2018 Slightech Co., Ltd. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#ifndef MYNTEYE_UTILS_FILES_H_
+#define MYNTEYE_UTILS_FILES_H_
 
 #include "mynteye/stubs/global.h"
+#include "mynteye/util/strings.h"
 
-#if defined(OS_WIN) && !defined(OS_MINGW) && !defined(OS_CYGWIN)
+#if defined(MYNTEYE_OS_WIN) && !defined(MYNTEYE_OS_MINGW) \
+    && !defined(MYNTEYE_OS_CYGWIN)
 #include <direct.h>
 #else
 #include <sys/stat.h>
@@ -16,13 +31,13 @@ MYNTEYE_BEGIN_NAMESPACE
 namespace files {
 
 bool _mkdir(const std::string &path) {
-#if defined(OS_MINGW) || defined(OS_CYGWIN)
+#if defined(MYNTEYE_OS_MINGW) || defined(MYNTEYE_OS_CYGWIN)
   const int status = ::mkdir(path.c_str());
-#elif defined(OS_WIN)
+#elif defined(MYNTEYE_OS_WIN)
   const int status = ::_mkdir(path.c_str());
 #else
   const int status =
-    ::mkdir(path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    mkdir(path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 #endif
 
   if (status != 0 && errno != EEXIST) {
@@ -32,7 +47,7 @@ bool _mkdir(const std::string &path) {
   }
   if (errno == EEXIST) {
     // std::cout << "Create directory needless"  <<
-    // "(already exist), path " << path << std::endl;
+    // "(already exist), path: " << path << std::endl;
     return true;
   } else {
     // std::cout << "Create directory success, path: " <<
@@ -61,4 +76,4 @@ bool mkdir(const std::string &path) {
 
 MYNTEYE_END_NAMESPACE
 
-#endif //MYNTEYE_FILES_H // NOLINT
+#endif  // MYNTEYE_UTILS_FILES_H_
