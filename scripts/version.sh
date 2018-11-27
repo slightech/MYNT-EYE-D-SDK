495c595c5fcf9c8c8eed2615b1ed6021104ee45e
John Zhao <johnzhao@slightech.com>
2018-11-27 20:51:34 +0800

    Fix version.sh

--- scripts/version.sh
+++ scripts/version.sh
@@ -4,7 +4,7 @@ BASE_DIR=$(cd "$(dirname "$0")" && pwd)
 ROOT_DIR=$(dirname "$BASE_DIR")
 CONFIG_FILE="$ROOT_DIR/CMakeLists.txt"
 
-version=$(cat "$CONFIG_FILE" | grep -m1 "mynteye VERSION")
+version=$(cat "$CONFIG_FILE" | grep -m1 "mynteyed VERSION")
 version=$(echo "${version%LANGUAGES*}")
 version=$(echo "${version#*VERSION}" | tr -d '[:space:]')
 
