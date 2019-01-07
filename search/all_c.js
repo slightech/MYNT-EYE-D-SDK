39440421d3e81fb6164d2e5864284c2f8c1d648c
John Zhao <johnzhao@slightech.com>
2019-01-07 12:45:43 +0800

    docs(gh-pages): update doc

--- search/all_c.js
+++ search/all_c.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['open',['Open',['../classmynteyed_1_1_camera.html#a8f088ced93a4f1d734ca84aef8120598',1,'mynteyed::Camera::Open()'],['../classmynteyed_1_1_camera.html#adfb443ba08e9bc613aef364cdb8a5137',1,'mynteyed::Camera::Open(const OpenParams &amp;params)']]],
-  ['openparams',['OpenParams',['../structmynteyed_1_1_open_params.html#a830b91a62f5a6026c3ee38f637362000',1,'mynteyed::OpenParams']]],
-  ['openparams',['OpenParams',['../structmynteyed_1_1_open_params.html',1,'mynteyed']]]
+  ['openparams',['OpenParams',['../structmynteyed_1_1_open_params.html',1,'mynteyed::OpenParams'],['../structmynteyed_1_1_open_params.html#a830b91a62f5a6026c3ee38f637362000',1,'mynteyed::OpenParams::OpenParams()']]]
 ];
