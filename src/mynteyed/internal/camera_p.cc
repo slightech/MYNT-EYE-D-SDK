863e0467a69bc9afed896b060d3408073f6de349
Osenberg <junyangqu@slightech.com>
2019-01-15 20:27:06 +0800

    fix(camera_p.cc): fixed can't get image parameters on panda device

--- src/mynteyed/internal/camera_p.cc
+++ src/mynteyed/internal/camera_p.cc
@@ -144,7 +144,7 @@ StreamIntrinsics CameraPrivate::GetStreamIntrinsics(
     const StreamMode& stream_mode, bool* ok) {
   StreamIntrinsics in;
   auto calib = GetCameraCalibration(stream_mode);
-  if (calib == nullptr || channels_->IsBetaDevice()) {
+  if (calib == nullptr || calib->InImgWidth == 0) {
     *ok = false;
     return std::move(in);
   }
