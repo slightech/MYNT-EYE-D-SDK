0e11236b1d063acbf1e597128c961a8460bed0b9
TinyO <tinysilk@hotmail.com>
2019-05-30 17:55:43 +0800

    fix(filter): fix sample display.

--- samples/src/get_depth_with_filter.cc
+++ samples/src/get_depth_with_filter.cc
@@ -38,88 +38,6 @@ class DepthRegion {
 
   ~DepthRegion() = default;
 
-  /**
-   * 鼠标事件：默认不选中区域，随鼠标移动而显示。单击后，则会选中区域来显示。你可以再单击已选中区域或双击未选中区域，取消选中。
-   */
-  void OnMouse(const int& event, const int& x, const int& y, const int& flags) {
-    if (event != cv::EVENT_MOUSEMOVE && event != cv::EVENT_LBUTTONDOWN) {
-      return;
-    }
-    show_ = true;
-
-    if (event == cv::EVENT_MOUSEMOVE) {
-      if (!selected_) {
-        point_.x = x;
-        point_.y = y;
-      }
-    } else if (event == cv::EVENT_LBUTTONDOWN) {
-      if (selected_) {
-        if (x >= static_cast<int>(point_.x - n_) &&
-            x <= static_cast<int>(point_.x + n_) &&
-            y >= static_cast<int>(point_.y - n_) &&
-            y <= static_cast<int>(point_.y + n_)) {
-          selected_ = false;
-        }
-      } else {
-        selected_ = true;
-      }
-      point_.x = x;
-      point_.y = y;
-    }
-  }
-
-  template<typename T>
-  void ShowElems(const cv::Mat& depth,
-      std::function<std::string(const T& elem)> elem2string,
-      int elem_space = 40,
-      std::function<std::string(const cv::Mat& depth, const cv::Point& point,
-          const std::uint32_t& n)> getinfo = nullptr) {
-    if (!show_) return;
-
-    int space = std::move(elem_space);
-    int n = 2 * n_ + 1;
-    cv::Mat im(space*n, space*n, CV_8UC3, cv::Scalar(255,255,255));
-
-    int x, y;
-    std::string str;
-    int baseline = 0;
-    for (int i = -n_; i <= n; ++i) {
-      x = point_.x + i;
-      if (x < 0 || x >= depth.cols) continue;
-      for (int j = -n_; j <= n; ++j) {
-        y = point_.y + j;
-        if (y < 0 || y >= depth.rows) continue;
-
-        str = elem2string(depth.at<T>(y, x));
-
-        cv::Scalar color(0,0,0);
-        if (i == 0 && j == 0) color = cv::Scalar(0,0,255);
-
-        cv::Size sz = cv::getTextSize(str,
-          cv::FONT_HERSHEY_PLAIN, 1, 1, &baseline);
-
-        cv::putText(im, str,
-          cv::Point((i+n_)*space + (space-sz.width)/2,
-            (j+n_)*space + (space+sz.height)/2),
-          cv::FONT_HERSHEY_PLAIN, 1, color, 1);
-      }
-    }
-
-    if (getinfo) {
-      std::string info = getinfo(depth, point_, n_);
-      if (!info.empty()) {
-        cv::Size sz = cv::getTextSize(info,
-          cv::FONT_HERSHEY_PLAIN, 1, 1, &baseline);
-
-        cv::putText(im, info,
-          cv::Point(5, 5 + sz.height),
-          cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(255,0,255), 1);
-      }
-    }
-
-    cv::imshow("region", im);
-  }
-
   void DrawRect(const cv::Mat &im) {
     if (!show_) return;
     std::uint32_t n = (n_ > 1) ? n_ : 1;
@@ -140,11 +58,6 @@ class DepthRegion {
   bool selected_;
   cv::Point point_;
 };
-
-void OnDepthMouseCallback(int event, int x, int y, int flags, void* userdata) {
-  DepthRegion* region = reinterpret_cast<DepthRegion*>(userdata);
-  region->OnMouse(event, x, y, flags);
-}
 
 }  // namespace
 
@@ -205,29 +118,7 @@ int main(int argc, char const* argv[]) {
 
   cout << "Press ESC/Q on Windows to terminate" << endl;
 
-  cv::namedWindow("color");
-  cv::namedWindow("depth");
-  cv::namedWindow("region");
-
   DepthRegion depth_region(3);
-  auto depth_info = [](
-      const cv::Mat& depth, const cv::Point& point, const std::uint32_t& n) {
-    /*
-    int row_beg = point.y - n, row_end = point.y + n + 1;
-    int col_beg = point.x - n, col_end = point.x + n + 1;
-    if (row_beg < 0) row_beg = 0;
-    if (row_end >= depth.rows) row_end = depth.rows;
-    if (col_beg < 0) col_beg = 0;
-    if (col_end >= depth.cols) col_end = depth.cols;
-    cout << "[" << point.y << ", " << point.x << "]" << endl
-      << depth.rowRange(row_beg, row_end).colRange(col_beg, col_end)
-      << endl << endl;
-    */
-    std::ostringstream os;
-    os << "depth pos: [" << point.y << ", " << point.x << "]"
-      << "±" << n << ", unit: mm";
-    return os.str();
-  };
 
   CVPainter painter;
   util::Counter counter;
@@ -238,8 +129,6 @@ int main(int argc, char const* argv[]) {
     auto image_depth = cam.GetStreamData(ImageType::IMAGE_DEPTH);
     if (image_depth.img) {
       cv::Mat depth = image_depth.img->To(ImageFormat::DEPTH_RAW)->ToMat();
-
-      cv::setMouseCallback("depth", OnDepthMouseCallback, &depth_region);
       // Note: DrawRect will change some depth values to show the rect.
       depth_region.DrawRect(depth);
       cv::Mat res_org;
@@ -259,10 +148,6 @@ int main(int argc, char const* argv[]) {
       cv::applyColorMap(res, res, cv::COLORMAP_JET);
 #endif
       cv::imshow("depth_after_filter", res);
-
-      depth_region.ShowElems<ushort>(depth, [](const ushort& elem) {
-        return std::to_string(elem);
-      }, 80, depth_info);
     }
 
     char key = static_cast<char>(cv::waitKey(1));
