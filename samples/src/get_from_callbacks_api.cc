1f23f644012852fc31a663be6e88a81104523d58
Messier <guhanbin@slightech.com>
2019-10-09 09:24:53 +0800

    feature(*): add api

---
+++ samples/src/get_from_callbacks_api.cc
@@ -0,0 +1,160 @@
+// Copyright 2018 Slightech Co., Ltd. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include <iostream>
+#include <mutex>
+
+#include <opencv2/highgui/highgui.hpp>
+
+#include "mynteyed/camera.h"
+#include "mynteyed/utils.h"
+
+#include "util/cam_utils.h"
+#include "util/counter.h"
+#include "util/cv_painter.h"
+
+MYNTEYE_USE_NAMESPACE
+
+int main(int argc, char const* argv[]) {
+  API api;
+  bool ok;
+  auto &&request = api.SelectStreamRequest(&ok);
+  request.framerate = 30;
+  request.stream_mode = StreamMode::STREAM_2560x720;
+
+  // Enable image infos
+  api.EnableImageInfo(false);
+  // Enable motion datas
+  api.EnableMotionDatas(0);
+
+  // Callbacks
+  std::mutex mutex;
+  {
+    // Set image info callback
+    api.SetImgInfoCallback([&mutex](const std::shared_ptr<ImgInfo>& info) {
+      std::lock_guard<std::mutex> _(mutex);
+      std::cout << "  [img_info] fid: " << info->frame_id
+          << ", stamp: " << info->timestamp
+          << ", expos: " << info->exposure_time << std::endl
+          << std::flush;
+    });
+
+    std::vector<Stream> types{
+      Stream::IMAGE_LEFT_COLOR,
+      Stream::IMAGE_RIGHT_COLOR,
+      Stream::IMAGE_DEPTH,
+    };
+    for (auto&& type : types) {
+      // Set stream data callback
+      api.SetStreamCallback(type, [&mutex](const StreamData& data) {
+        std::lock_guard<std::mutex> _(mutex);
+        std::cout << "  [" << data.img->type() << "] fid: "
+            << data.img->frame_id() << std::endl
+            << std::flush;
+      });
+    }
+
+    // Set motion data callback
+    api.SetMotionCallback([&mutex](const MotionData& data) {
+      std::lock_guard<std::mutex> _(mutex);
+      if (data.imu->flag == MYNTEYE_IMU_ACCEL) {
+        std::cout << "[accel] stamp: " << data.imu->timestamp
+          << ", x: " << data.imu->accel[0]
+          << ", y: " << data.imu->accel[1]
+          << ", z: " << data.imu->accel[2]
+          << ", temp: " << data.imu->temperature
+          << std::endl;
+      } else if (data.imu->flag == MYNTEYE_IMU_GYRO) {
+        std::cout << "[gyro] stamp: " << data.imu->timestamp
+          << ", x: " << data.imu->gyro[0]
+          << ", y: " << data.imu->gyro[1]
+          << ", z: " << data.imu->gyro[2]
+          << ", temp: " << data.imu->temperature
+          << std::endl;
+      }
+      std::cout << std::flush;
+    });
+  }
+
+  api.ConfigStreamRequest(request);
+
+  std::cout << std::endl;
+  if (!api.IsOpened()) {
+    std::cerr << "Error: Open camera failed" << std::endl;
+    return 1;
+  }
+  std::cout << "Open device success" << std::endl << std::endl;
+
+  std::cout << "Press ESC/Q on Windows to terminate" << std::endl;
+
+  bool is_left_ok = api.Supports(Stream::IMAGE_LEFT_COLOR);
+  bool is_right_ok = api.Supports(Stream::IMAGE_RIGHT_COLOR);
+  bool is_depth_ok = api.Supports(Stream::IMAGE_DEPTH);
+
+  if (is_left_ok) cv::namedWindow("left color");
+  if (is_right_ok) cv::namedWindow("right color");
+  if (is_depth_ok) cv::namedWindow("depth");
+
+  CVPainter painter;
+  util::Counter counter;
+  for (;;) {
+    api.WaitForStreams();
+    counter.Update();
+
+    if (is_left_ok) {
+      auto left_color = api.GetStreamData(Stream::IMAGE_LEFT_COLOR);
+      if (left_color.img) {
+        cv::Mat left = left_color.img->To(ImageFormat::COLOR_BGR)->ToMat();
+        painter.DrawSize(left, CVPainter::TOP_LEFT);
+        painter.DrawStreamData(left, left_color, CVPainter::TOP_RIGHT);
+        painter.DrawInformation(left, util::to_string(counter.fps()),
+            CVPainter::BOTTOM_RIGHT);
+        cv::imshow("left color", left);
+      }
+    }
+
+    if (is_right_ok) {
+      auto right_color = api.GetStreamData(Stream::IMAGE_RIGHT_COLOR);
+      if (right_color.img) {
+        cv::Mat right = right_color.img->To(ImageFormat::COLOR_BGR)->ToMat();
+        painter.DrawSize(right, CVPainter::TOP_LEFT);
+        painter.DrawStreamData(right, right_color, CVPainter::TOP_RIGHT);
+        cv::imshow("right color", right);
+      }
+    }
+
+    if (is_depth_ok) {
+      auto image_depth = api.GetStreamData(Stream::IMAGE_DEPTH);
+      if (image_depth.img) {
+        cv::Mat depth;
+        if (request.depth_mode == DepthMode::DEPTH_COLORFUL) {
+          depth = image_depth.img->To(ImageFormat::DEPTH_BGR)->ToMat();
+        } else {
+          depth = image_depth.img->ToMat();
+        }
+        painter.DrawSize(depth, CVPainter::TOP_LEFT);
+        painter.DrawStreamData(depth, image_depth, CVPainter::TOP_RIGHT);
+        cv::imshow("depth", depth);
+      }
+    }
+
+    char key = static_cast<char>(cv::waitKey(1));
+    if (key == 27 || key == 'q' || key == 'Q') {  // ESC/Q
+      break;
+    }
+  }
+
+  api.Close();
+  cv::destroyAllWindows();
+  return 0;
+}
--- src/mynteyed/camera.cc
+++ src/mynteyed/camera.cc
@@ -15,293 +15,435 @@
 
 #include "mynteyed/internal/camera_p.h"
 #include "mynteyed/util/log.h"
+#include <iomanip>
 
 MYNTEYE_USE_NAMESPACE
 
-Camera::Camera() : p_(new CameraPrivate()) {
+Camera::Camera() : p_(new CameraPrivate())
+{
   DBG_LOGD(__func__);
 }
 
-Camera::~Camera() {
+Camera::~Camera()
+{
   DBG_LOGD(__func__);
   p_.release();
 }
 
-std::vector<DeviceInfo> Camera::GetDeviceInfos() const {
+std::vector<DeviceInfo> Camera::GetDeviceInfos() const
+{
   std::vector<DeviceInfo> device_infos;
   GetDeviceInfos(&device_infos);
   return device_infos;
 }
 
-void Camera::GetDeviceInfos(std::vector<DeviceInfo>* dev_infos) const {
+void Camera::GetDeviceInfos(std::vector<DeviceInfo> *dev_infos) const
+{
   p_->GetDeviceInfos(dev_infos);
 }
 
-void Camera::GetStreamInfos(const std::int32_t& dev_index,
-    std::vector<StreamInfo>* color_infos,
-    std::vector<StreamInfo>* depth_infos) const {
+void Camera::GetStreamInfos(const std::int32_t &dev_index,
+                            std::vector<StreamInfo> *color_infos,
+                            std::vector<StreamInfo> *depth_infos) const
+{
   p_->GetStreamInfos(dev_index, color_infos, depth_infos);
 }
 
-ErrorCode Camera::Open() {
+ErrorCode Camera::Open()
+{
   std::vector<DeviceInfo> dev_infos = GetDeviceInfos();
-  if (dev_infos.size() <= 0) {
+  if (dev_infos.size() <= 0)
+  {
     LOGE("Error: Device not found");
     return ErrorCode::ERROR_CAMERA_OPEN_FAILED;
   }
   return Open(OpenParams(0));
 }
 
-ErrorCode Camera::Open(const OpenParams& params) {
+ErrorCode Camera::Open(const OpenParams &params)
+{
   return p_->Open(params);
 }
 
-bool Camera::IsOpened() const {
+bool Camera::IsOpened() const
+{
   return p_->IsOpened();
 }
 
-OpenParams Camera::GetOpenParams() const {
+OpenParams Camera::GetOpenParams() const
+{
   return p_->GetOpenParams();
 }
 
-std::shared_ptr<device::Descriptors> Camera::GetDescriptors() const {
+std::shared_ptr<device::Descriptors> Camera::GetDescriptors() const
+{
   return p_->GetDescriptors();
 }
 
-std::string Camera::GetDescriptor(const Descriptor &desc) const {
+std::string Camera::GetDescriptor(const Descriptor &desc) const
+{
   return p_->GetDescriptor(desc);
 }
 
 StreamIntrinsics Camera::GetStreamIntrinsics(
-    const StreamMode& stream_mode, bool* ok) const {
+    const StreamMode &stream_mode, bool *ok) const
+{
   auto in = p_->GetStreamIntrinsics(stream_mode, ok);
-  if (*ok) return in;
+  if (*ok)
+    return in;
   // if false, return default intrinsics
   // {w, h, fx, fy, cx, cy, coeffs[5]{k1,k2,p1,p2,k3}}
   CameraIntrinsics cam_in;
-  switch (stream_mode) {
-    case StreamMode::STREAM_640x480:
-      cam_in = {640, 480, 979.8, 942.8, 682.3 / 2, 254.9, {0, 0, 0, 0, 0}};
-    case StreamMode::STREAM_1280x480:
-      cam_in = {640, 480, 979.8, 942.8, 682.3, 254.9, {0, 0, 0, 0, 0}};
-    case StreamMode::STREAM_1280x720:
-      cam_in = {1280, 720, 979.8, 942.8, 682.3, 254.9 * 2, {0, 0, 0, 0, 0}};
-    case StreamMode::STREAM_2560x720:
-      cam_in = {1280, 720, 979.8, 942.8, 682.3 * 2, 254.9 * 2, {0, 0, 0, 0, 0}};
-    default:
-      cam_in = {1280, 720, 979.8, 942.8, 682.3, 254.9 * 2, {0, 0, 0, 0, 0}};
+  switch (stream_mode)
+  {
+  case StreamMode::STREAM_640x480:
+    cam_in = {640, 480, 979.8, 942.8, 682.3 / 2, 254.9, {0, 0, 0, 0, 0}};
+  case StreamMode::STREAM_1280x480:
+    cam_in = {640, 480, 979.8, 942.8, 682.3, 254.9, {0, 0, 0, 0, 0}};
+  case StreamMode::STREAM_1280x720:
+    cam_in = {1280, 720, 979.8, 942.8, 682.3, 254.9 * 2, {0, 0, 0, 0, 0}};
+  case StreamMode::STREAM_2560x720:
+    cam_in = {1280, 720, 979.8, 942.8, 682.3 * 2, 254.9 * 2, {0, 0, 0, 0, 0}};
+  default:
+    cam_in = {1280, 720, 979.8, 942.8, 682.3, 254.9 * 2, {0, 0, 0, 0, 0}};
   }
   return {cam_in, cam_in};
 }
 
 StreamExtrinsics Camera::GetStreamExtrinsics(
-    const StreamMode& stream_mode, bool* ok) const {
+    const StreamMode &stream_mode, bool *ok) const
+{
   auto ex = p_->GetStreamExtrinsics(stream_mode, ok);
-  if (*ok) return ex;
+  if (*ok)
+    return ex;
 
   return {{0.99978113174438477, 0.00282573699951172, 0.02072787284851074,
-    -0.00284624099731445, 0.99999547004699707, 0.00096035003662109,
-    -0.02072513103485107, -0.00101912021636963, 0.99978458881378174},
-    {-120.21408843994140625, 0.00000000000000000, 0.00000000000000000}};
+           -0.00284624099731445, 0.99999547004699707, 0.00096035003662109,
+           -0.02072513103485107, -0.00101912021636963, 0.99978458881378174},
+          {-120.21408843994140625, 0.00000000000000000, 0.00000000000000000}};
 }
 
-bool Camera::WriteCameraCalibrationBinFile(const std::string& filename) {
+bool Camera::WriteCameraCalibrationBinFile(const std::string &filename)
+{
   return p_->WriteCameraCalibrationBinFile(filename);
 }
 
-MotionIntrinsics Camera::GetMotionIntrinsics(bool* ok) const {
+MotionIntrinsics Camera::GetMotionIntrinsics(bool *ok) const
+{
   return p_->GetMotionIntrinsics(ok);
 }
 
-MotionExtrinsics Camera::GetMotionExtrinsics(bool* ok) const {
+MotionExtrinsics Camera::GetMotionExtrinsics(bool *ok) const
+{
   return p_->GetMotionExtrinsics(ok);
 }
 
-bool Camera::IsWriteDeviceSupported() const {
+bool Camera::IsWriteDeviceSupported() const
+{
   return p_->IsWriteDeviceSupported();
 }
 
 bool Camera::WriteDeviceFlash(
     device::Descriptors *desc,
     device::ImuParams *imu_params,
-    Version *spec_version) {
+    Version *spec_version)
+{
   return p_->WriteDeviceFlash(desc, imu_params, spec_version);
 }
 
-void Camera::EnableProcessMode(const ProcessMode& mode) {
+void Camera::EnableProcessMode(const ProcessMode &mode)
+{
   p_->EnableProcessMode(mode);
 }
 
-void Camera::EnableProcessMode(const std::int32_t& mode) {
+void Camera::EnableProcessMode(const std::int32_t &mode)
+{
   p_->EnableProcessMode(mode);
 }
 
-bool Camera::IsImageInfoSupported() const {
+bool Camera::IsImageInfoSupported() const
+{
   return p_->IsImageInfoSupported();
 }
 
-void Camera::EnableImageInfo(bool sync) {
+void Camera::EnableImageInfo(bool sync)
+{
   p_->EnableImageInfo(sync);
 }
 
-void Camera::DisableImageInfo() {
+void Camera::DisableImageInfo()
+{
   p_->DisableImageInfo();
 }
 
-bool Camera::IsImageInfoEnabled() const {
+bool Camera::IsImageInfoEnabled() const
+{
   return p_->IsImageInfoEnabled();
 }
 
-bool Camera::IsImageInfoSynced() const {
+bool Camera::IsImageInfoSynced() const
+{
   return p_->IsImageInfoSynced();
 }
 
-bool Camera::IsStreamDataEnabled(const ImageType& type) const {
+bool Camera::IsStreamDataEnabled(const ImageType &type) const
+{
   return p_->IsStreamDataEnabled(type);
 }
 
-bool Camera::HasStreamDataEnabled() const {
+bool Camera::HasStreamDataEnabled() const
+{
   return p_->HasStreamDataEnabled();
 }
 
-StreamData Camera::GetStreamData(const ImageType& type) {
+StreamData Camera::GetStreamData(const ImageType &type)
+{
   return std::move(p_->GetStreamData(type));
 }
 
-std::vector<StreamData> Camera::GetStreamDatas(const ImageType& type) {
+std::vector<StreamData> Camera::GetStreamDatas(const ImageType &type)
+{
   return std::move(p_->GetStreamDatas(type));
 }
 
-bool Camera::IsMotionDatasSupported() const {
+bool Camera::IsMotionDatasSupported() const
+{
   return p_->IsExSensorDatasSupported();
 }
 
-void Camera::EnableMotionDatas(std::size_t max_size) {
+void Camera::EnableMotionDatas(std::size_t max_size)
+{
   p_->EnableMotionDatas(std::move(max_size));
 }
 
-void Camera::DisableMotionDatas() {
+void Camera::DisableMotionDatas()
+{
   p_->DisableMotionDatas();
 }
 
-bool Camera::IsMotionDatasEnabled() const {
+bool Camera::IsMotionDatasEnabled() const
+{
   return p_->IsMotionDatasEnabled();
 }
 
-std::vector<MotionData> Camera::GetMotionDatas() {
+std::vector<MotionData> Camera::GetMotionDatas()
+{
   return std::move(p_->GetMotionDatas());
 }
 
-void Camera::SetImgInfoCallback(img_info_callback_t callback, bool async) {
+void Camera::SetImgInfoCallback(img_info_callback_t callback, bool async)
+{
   p_->SetImgInfoCallback(callback, async);
 }
 
-void Camera::SetStreamCallback(const ImageType& type,
-    stream_callback_t callback, bool async) {
+void Camera::SetStreamCallback(const ImageType &type,
+                               stream_callback_t callback, bool async)
+{
   p_->SetStreamCallback(type, callback, async);
 }
 
-void Camera::SetMotionCallback(motion_callback_t callback, bool async) {
+void Camera::SetMotionCallback(motion_callback_t callback, bool async)
+{
   p_->SetMotionCallback(callback, async);
 }
 
-void Camera::Close() {
+void Camera::Close()
+{
   p_->Close();
 }
 
-void Camera::SetExposureTime(const float &value) {
+void Camera::SetExposureTime(const float &value)
+{
   return p_->SetExposureTime(value);
 }
 
-void Camera::GetExposureTime(float &value) {
+void Camera::GetExposureTime(float &value)
+{
   return p_->GetExposureTime(value);
 }
 
-void Camera::SetGlobalGain(const float &value) {
+void Camera::SetGlobalGain(const float &value)
+{
   return p_->SetGlobalGain(value);
 }
 
-void Camera::GetGlobalGain(float &value) {
+void Camera::GetGlobalGain(float &value)
+{
   return p_->GetGlobalGain(value);
 }
 
 void Camera::SetIRIntensity(
-    const std::uint16_t &value) {
+    const std::uint16_t &value)
+{
   return p_->SetIRIntensity(value);
 }
 
-bool Camera::AutoExposureControl(bool enable) {
+bool Camera::AutoExposureControl(bool enable)
+{
   return p_->AutoExposureControl(enable);
 }
 
-bool Camera::AutoWhiteBalanceControl(bool enable) {
+bool Camera::AutoWhiteBalanceControl(bool enable)
+{
   return p_->AutoWhiteBalanceControl(enable);
 }
 
 #ifdef MYNTEYE_DEPRECATED_COMPAT
-void Camera::EnableStreamData(const ImageType& type) {
+void Camera::EnableStreamData(const ImageType &type)
+{
   LOGW("%s is deprecated, replaced by OpenParams#device_mode.", __func__);
 }
 
-void Camera::DisableStreamData(const ImageType& type) {
+void Camera::DisableStreamData(const ImageType &type)
+{
   LOGW("%s is deprecated, replaced by OpenParams#device_mode.", __func__);
 }
 #endif
 
-bool Camera::IsLocationDatasSupported() const {
+bool Camera::IsLocationDatasSupported() const
+{
   return p_->IsExSensorDatasSupported();
 }
 
-void Camera::EnableLocationDatas(std::size_t max_size) {
+void Camera::EnableLocationDatas(std::size_t max_size)
+{
   p_->EnableLocationDatas(std::move(max_size));
 }
 
-void Camera::DisableLocationDatas() {
+void Camera::DisableLocationDatas()
+{
   p_->DisableLocationDatas();
 }
 
-bool Camera::IsLocationDatasEnabled() const {
+bool Camera::IsLocationDatasEnabled() const
+{
   return p_->IsLocationDatasEnabled();
 }
 
-std::vector<LocationData> Camera::GetLocationDatas() {
+std::vector<LocationData> Camera::GetLocationDatas()
+{
   return std::move(p_->GetLocationDatas());
 }
 
-bool Camera::IsDistanceDatasSupported() const {
+bool Camera::IsDistanceDatasSupported() const
+{
   return p_->IsExSensorDatasSupported();
 }
 
-void Camera::EnableDistanceDatas(std::size_t max_size) {
+void Camera::EnableDistanceDatas(std::size_t max_size)
+{
   p_->EnableDistanceDatas(std::move(max_size));
 }
 
-void Camera::DisableDistanceDatas() {
+void Camera::DisableDistanceDatas()
+{
   p_->DisableDistanceDatas();
 }
 
-bool Camera::IsDistanceDatasEnabled() const {
+bool Camera::IsDistanceDatasEnabled() const
+{
   return p_->IsDistanceDatasEnabled();
 }
 
-std::vector<DistanceData> Camera::GetDistanceDatas() {
+std::vector<DistanceData> Camera::GetDistanceDatas()
+{
   return std::move(p_->GetDistanceDatas());
 }
 
-void Camera::WaitForStream() {
+void Camera::WaitForStream()
+{
   return p_->WaitForStream();
 }
 
-bool Camera::AuxiliaryChipFirmwareUpdate(const char* filepath) {
+bool Camera::AuxiliaryChipFirmwareUpdate(const char *filepath)
+{
   return p_->AuxiliaryChipFirmwareUpdate(filepath);
 }
 
 #ifdef MYNTEYE_DEPRECATED_COMPAT
-bool Camera::HidFirmwareUpdate(const char* filepath) {
+bool Camera::HidFirmwareUpdate(const char *filepath)
+{
   return AuxiliaryChipFirmwareUpdate(filepath);
 }
 #endif
 
-void Camera::ControlReconnectStatus(const bool &status) {
+void Camera::ControlReconnectStatus(const bool &status)
+{
   p_->ControlReconnectStatus(status);
+}
+
+StreamRequest Camera::SelectStreamRequest(bool *ok) const
+{
+  DeviceInfo dev_info;
+  const Camera &cam = *this;
+  StreamRequest params;
+  
+// select
+  std::cout << std::endl;
+  std::string dashes(80, '-');
+
+  std::vector<DeviceInfo> dev_infos = this->GetDeviceInfos();
+  size_t n = dev_infos.size();
+  if (n <= 0) {
+    std::cerr << "Error: Device not found" << std::endl;
+    *ok = false;
+    return params;
+  }
+
+  std::cout << dashes << std::endl;
+  std::cout << "Index | Device Information" << std::endl;
+  std::cout << dashes << std::endl;
+  for (auto &&info : dev_infos) {
+    std::cout << std::setw(5) << info.index << " | " << info << std::endl;
+  }
+  std::cout << dashes << std::endl;
+
+  if (n == 1) {
+    dev_info = dev_infos[0];
+    std::cout << "Auto select a device to open, index: 0" << std::endl;
+  } else {
+    size_t i;
+    std::cout << "Please select a device to open, index: ";
+    std::cin >> i;
+    std::cout << std::endl;
+    if (i < 0 || i >= n) {
+      std::cerr << "Error: Index out of range" << std::endl;
+      *ok = false;
+      return params;
+    }
+    dev_info = dev_infos[i];
+  }
+
+  uint32_t dev_index = dev_info.index;
+// print
+
+  std::vector<StreamInfo> color_infos;
+  std::vector<StreamInfo> depth_infos;
+  cam.GetStreamInfos(dev_index, &color_infos, &depth_infos);
+
+  std::cout << dashes << std::endl;
+  std::cout << "Index | Color Stream Information" << std::endl;
+  std::cout << dashes << std::endl;
+  for (auto &&info : color_infos) {
+    std::cout << std::setw(5) << info.index << " | " << info << std::endl;
+  }
+
+  std::cout << dashes << std::endl;
+  std::cout << "Index | Depth Stream Information" << std::endl;
+  std::cout << dashes << std::endl;
+  for (auto &&info : depth_infos) {
+    std::cout << std::setw(5) << info.index << " | " << info << std::endl;
+  }
+  std::cout << dashes << std::endl;
+
+  std::cout << "Open device: " << dev_info.index << ", "
+            << dev_info.name << std::endl
+            << std::endl;
+
+  params.framerate = 10;
+  params.depth_mode = DepthMode::DEPTH_RAW;
+  params.stream_mode = StreamMode::STREAM_1280x720;
+  params.ir_intensity = 4;
+  *ok = true;
+  return params;
 }
